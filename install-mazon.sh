2fe5a530f0b25f9c0f229fc7dac5ee7206235817
vcatafesta <vcatafesta@gmail.com>
2019-02-24 18:31:44 -0400

    install-mazon.sh

--- install-mazon.sh
+++ install-mazon.sh
@@ -633,7 +633,12 @@ function sh_wgetdefault(){
 			wget -c $URL 2>&1 | \
 		    	stdbuf -o0 awk '/[.] +[0-9][0-9]?[0-9]?%/ { print substr($0,63,3) }' | \
         		dialog --title "$plswait" --backtitle "$ccabec" --gauge $URL 7 70
-			;;
+				sh_testsha256sum
+				if [ $? = $false ]; then
+					info "\nOps, Pacote corrompido."
+					menuinstall
+				fi
+		;;
 
 			sh_testsha256sum
 			if [ $? = $false ]; then
